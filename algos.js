62681bc4300aec373a83239b275811e7fde7b926
Jake <mjakerick@gmail.com>
2023-01-06 16:11:13 -0800

    Two Sum

--- algos.js
+++ algos.js
@@ -435,3 +435,25 @@
 // }
 //
 // console.log(hasVowels(words));
+
+
+////////////////////////////
+// Two Sum
+////////////////////////////
+
+// // We want to return the position of the two numbers whose sum is equivalent to the target
+//
+// function twoSum(nums, target) {
+//   let answer = [];
+//   for (i = 0; i < nums.length; i++){
+//     for (j = i + 1; j < nums.length; i++){
+//       if (nums[i] + nums[j] === target){
+//         answer.push(i, j)
+//         return answer
+//       }
+//     }
+//   }
+//   return answer
+// }
+//
+// console.log(twoSum([2, 7, 11, 15], 9));

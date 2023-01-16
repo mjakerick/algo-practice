08a7c6081cfee7cf5c0016c474f6e7a37549e3f0
Jake <mjakerick@gmail.com>
2023-01-15 18:00:15 -0800

    14. Longest Common Prefix

--- algos.js
+++ algos.js
@@ -525,3 +525,47 @@
 // }
 //
 // console.log(romanToInt("MCMXCIV"));
+
+
+////////////////////////////
+// 14. Longest Common Prefix
+////////////////////////////
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+//
+// If there is no common prefix, return an empty string "".
+//
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+//
+// Constraints:
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+function longestCommonPrefix(strs) {
+
+let answer = "";
+
+  for (i = 0; i < strs[i].length; i++) {
+
+    let letter = strs[0][i]
+
+    for (j = 0; j < strs.length; j++) {
+      if (strs[j][i] != letter) {
+        return answer
+
+      }
+    }
+    answer += letter;
+  }
+  return answer
+};
+
+console.log(longestCommonPrefix(["dog","racecar","car"]));

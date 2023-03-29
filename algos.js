ab1d2525f985760ac8843da9c8d66bb2eef83fff
Jake <mjakerick@gmail.com>
2023-03-29 18:48:17 -0500

    28. Find the Index of the First Occurrence in a String

--- algos.js
+++ algos.js
@@ -927,8 +927,37 @@ function lengthOfLongestSubstring(s) {
 // 28. Find the Index of the First Occurrence in a String
 ////////////////////////////
 
-function strStr(haystack, needle) {
-  let ans = -1;
+// sad way to solve using built in functions
 
+// function strStr(haystack, needle) {
+//   if(haystack.includes(needle)) {
+//     return haystack.indexOf(needle);
+//   } else {
+//     return -1;
+//   }
+// }
+//
+// console.log(strStr("sadbutsad", "sad"));
 
-}
+// Interview way to solve with iteration
+
+// const strStr = (haystack, needle) => {
+//   if (needle === '' || needle === haystack) return 0;    // the only mandatory check here is (needle === '')
+//   if (haystack.length < needle.length) return -1;        // the other ones are for efficiency
+//
+//   for (let i = 0; i < haystack.length - needle.length + 1; i++) {    // start looping through haystack until the remaining substring is shorter than needle
+//     if (haystack[i] === needle[0]) {                // as soon as we see a character that matches the first character of needle
+//       for (let j = 0; j < needle.length; j++) {     // start looping through both needle and haystack
+//         if (needle[j] !== haystack[i + j]) {        // as soon as the characters don't match
+//           break;                                    // break out of the loop and return to looping through haystack
+//         } else if (j === needle.length - 1){        // otherwise, if we looped through the entire needle and all of the characters matched
+//           return i;                                 // return the index of the first character of haystack with which we started the loop
+//         }
+//       }
+//     }
+//   }
+//
+//   return -1;                                        // return -1 if there wasn't a match
+// };
+//
+// console.log(strStr("sadbutsad", "sad"));

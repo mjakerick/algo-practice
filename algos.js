a09aa37bb4b0649502d24329af09f8eb97d0a7ab
Jake <mjakerick@gmail.com>
2023-04-02 11:53:36 -0500

    66. Plus One

--- algos.js
+++ algos.js
@@ -961,3 +961,25 @@ function lengthOfLongestSubstring(s) {
 // };
 //
 // console.log(strStr("sadbutsad", "sad"));
+
+
+////////////////////////////
+// 66. Plus One
+////////////////////////////
+
+function plusOne(digits) {
+
+  for(i = digits.length - 1; i >= 0; i--){
+    if(digits[i] < 9){
+      digits[i] = digits[i] + 1;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]);

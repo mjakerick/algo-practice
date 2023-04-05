585af133c217b98b3406a7fa3d64793591ab20fb
Jake <mjakerick@gmail.com>
2023-04-04 19:56:33 -0500

    2

--- leetcode-75.js
+++ leetcode-75.js
@@ -1,6 +1,6 @@
-////////////////////////////
-// 1. Two Sum
-////////////////////////////
+//////////////////////////////////
+// 1. // 1. Two Sum
+//////////////////////////////////
 
 // function twoSum(nums, target) {
 //
@@ -15,5 +15,32 @@
 //   }
 //   return ans;
 // };
-// 
+//
 // console.log(twoSum([3,3], 6));
+
+
+//////////////////////////////////
+// 2. // 121. Best Time to Buy and Sell Stock
+//////////////////////////////////
+
+function maxProfit(prices) {
+
+  let buy = 0;
+  let sell = 1;
+  let maxP = 0;
+
+  while(sell < prices.length) {
+    if(prices[buy] < prices[sell]){
+      let profit = prices[sell] - prices[buy];
+      maxP = Math.max(maxP, profit);
+    } else {
+      buy = sell;
+    }
+    sell++;
+  }
+  return maxP;
+}
+
+console.log(maxProfit([7,1,5,3,6,4]));
+
+// output = 5 because 6 - 1 = 5

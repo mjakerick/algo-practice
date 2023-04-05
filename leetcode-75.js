67f9a7f7d63e93b9eef08d810821760651ca8d88
Jake <mjakerick@gmail.com>
2023-04-04 19:33:05 -0500

    1

---
+++ leetcode-75.js
@@ -0,0 +1,19 @@
+////////////////////////////
+// 1. Two Sum
+////////////////////////////
+
+// function twoSum(nums, target) {
+//
+//   let ans = [];
+//
+//   for(i = 0; i < nums.length; i++) {
+//     for(j = i + 1; j < nums.length; j++) {
+//       if(nums[i] + nums[j] == target) {
+//         ans.push(i, j);
+//       }
+//     }
+//   }
+//   return ans;
+// };
+// 
+// console.log(twoSum([3,3], 6));

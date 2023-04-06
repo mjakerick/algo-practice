ba8cdb2a7d37ed020b191b2e2c0b20ec30dea5b9
Jake <mjakerick@gmail.com>
2023-04-06 18:05:00 -0500

    3

--- leetcode-75.js
+++ leetcode-75.js
@@ -23,24 +23,46 @@
 // 2. // 121. Best Time to Buy and Sell Stock
 //////////////////////////////////
 
-function maxProfit(prices) {
+// function maxProfit(prices) {
+//
+//   let buy = 0;
+//   let sell = 1;
+//   let maxP = 0;
+//
+//   while(sell < prices.length) {
+//     if(prices[buy] < prices[sell]){
+//       let profit = prices[sell] - prices[buy];
+//       maxP = Math.max(maxP, profit);
+//     } else {
+//       buy = sell;
+//     }
+//     sell++;
+//   }
+//   return maxP;
+// }
+//
+// console.log(maxProfit([7,1,5,3,6,4]));
 
-  let buy = 0;
-  let sell = 1;
-  let maxP = 0;
 
-  while(sell < prices.length) {
-    if(prices[buy] < prices[sell]){
-      let profit = prices[sell] - prices[buy];
-      maxP = Math.max(maxP, profit);
-    } else {
-      buy = sell;
-    }
-    sell++;
-  }
-  return maxP;
-}
+//////////////////////////////////
+// 3. // 217. Contains Duplicate
+//////////////////////////////////
 
-console.log(maxProfit([7,1,5,3,6,4]));
+// function containsDuplicate(nums) {
+//   const hashset = new Set();
+//
+//   for(i = 0; i < nums.length; i++) {
+//     if(hashset.has(nums[i])) {
+//       return true;
+//     }
+//     hashset.add(nums[i])
+//   }
+//   return false;
+// }
+//
+// console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
 
-// output = 5 because 6 - 1 = 5
+
+//////////////////////////////////
+// 4. //
+//////////////////////////////////

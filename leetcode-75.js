b2bb9fb93509cfcae1dafeabe9dc97669bbe46c8
Jake <mjakerick@gmail.com>
2023-04-11 21:48:53 -0500

    9

--- leetcode-75.js
+++ leetcode-75.js
@@ -170,3 +170,39 @@
 //////////////////////////////////
 // 8. // 33. Search in Rotated Sorted Array
 //////////////////////////////////
+
+// function search(nums, target) {
+//   let left = 0;
+//   let right = nums.length - 1;
+//
+//   while(left <= right) {
+//     let mid = Math.floor((left + right) / 2);
+//     if(target == nums[mid]) {
+//       return mid;
+//     }
+//
+//     // left sorted portion
+//     if(nums[left] <= nums[mid]) {
+//       if(target > nums[mid] || target < nums[left]) {
+//         left = mid + 1;
+//       } else {
+//         right = mid - 1;
+//       }
+//       // right sorted portion
+//     } else {
+//       if(target < nums[mid] || target > nums[right]) {
+//         right = mid - 1;
+//       } else {
+//         left = mid + 1;
+//       }
+//     }
+//   }
+//   return -1;
+// }
+//
+// console.log(search([4,5,6,7,0,1,2], 0));
+
+
+//////////////////////////////////
+// 9. // 15. 3Sum
+//////////////////////////////////

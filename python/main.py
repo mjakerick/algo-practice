93066cdaad2931282630c0fd5cce4a9c3bbe9bf6
Jake <mjakerick@gmail.com>
2023-09-07 10:13:16 -0500

    arrays, also added leetcode75.py

--- python/main.py
+++ python/main.py
@@ -205,3 +205,162 @@
 # # # # # # # # # #
 # ARRAYS
 # # # # # # # # # #
+
+# # Arrays (called lists in python)
+# arr = [1, 2, 3]
+# print(arr)
+# # result is
+# # [1, 2, 3]
+
+# # Can be used as a stack
+# arr.append(4)
+# arr.append(5)
+# print(arr)
+# # result is
+# # [1, 2, 3, 4, 5]
+
+# arr.pop()
+# print(arr)
+# # result is
+# # [1, 2, 3, 4]
+
+# arr.insert(1, 7)
+# print(arr)
+# # result is
+# # [1, 7, 2, 3, 4]
+
+# arr[0] = 0
+# arr[3] = 0
+# print(arr)
+# # result is
+# # [0, 7, 2, 0, 4]
+
+# # Initialize arr of size n with default value of 1
+# n = 5
+# arr = [1] * n
+# print(arr)
+# print(len(arr))
+# # result is
+# # [1, 1, 1, 1, 1]
+# # 5
+
+# # Careful: -1 is not out of bounds, it's the last value
+# arr = [1, 2, 3]
+# print(arr[-1])
+# # result is
+# # 3
+
+# # Indexing -2 is the second to last value, etc.
+# print(arr[-2])
+# # result is
+# # 2
+
+# # Sublists (aka slicing)
+# arr = [1, 2, 3, 4]
+# print(arr[1:3])
+# # result is
+# # [2, 3]
+
+# # Similar to for-loop ranges, last index is non-inclusive
+# print(arr[0:4])
+# # result is
+# # [1, 2, 3, 4]
+
+# # But no out of bounds error
+# print(arr[0:10])
+# # result is
+# # [1, 2, 3, 4]
+
+# # Unpacking
+# a, b, c = [1, 2, 3]
+# print(a, b, c)
+# # result is
+# # 1 2 3
+
+# # Be careful though
+# # a, b = [1, 2, 3]
+
+# # Loop through arrays
+# nums = [1, 2, 3]
+
+# # Using index
+# for i in range(len(nums)):
+#     print(nums[i])
+# # result is
+# # 1
+# # 2
+# # 3
+
+# # Without index
+# for n in nums:
+#     print(n)
+# # result is
+# # 1
+# # 2
+# # 3
+
+# # With index and value
+# for i, n in enumerate(nums):
+#     print(i, n)
+# # result is
+# # 0 1
+# # 1 2
+# # 2 3
+
+# # Loop through multiple arrays simultaneously with unpacking
+# nums1 = [1, 3, 5]
+# nums2 = [2, 4, 6]
+# for n1, n2 in zip(nums1, nums2):
+#     print(n1, n2)
+# # result is
+# # 1 2
+# # 3 4
+# # 5 6
+
+# # Reverse
+# nums = [1, 2, 3]
+# nums.reverse()
+# print(nums)
+# # result is
+# # [3, 2, 1]
+
+# # Sorting
+# arr = [5, 4, 7, 3, 8]
+# arr.sort()
+# print(arr)
+# # result is
+# [3, 4, 5, 7, 8]
+
+# arr.sort(reverse=True)
+# print(arr)
+# # result is
+# [8, 7, 5, 4, 3]
+
+# arr = ["bob", "alice", "jane", "doe"]
+# arr.sort()
+# print(arr)
+# # result is
+# # ['alice', 'bob', 'doe', 'jane']
+
+# # Custom sort (by length of string)
+# arr.sort(key=lambda x: len(x))
+# print(arr)
+# # result is
+# # ['bob', 'doe', 'jane', 'alice']
+
+# # List comprehension
+# arr = [i for i in range(5)]
+# print(arr)
+# # result is
+# # [0, 1, 2, 3, 4]
+
+# # 2-D lists
+# arr = [[0] * 4 for i in range(4)]
+# print(arr)
+# print(arr[0][0], arr[3][3])
+# # result is
+# # [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+# # 0 0 this is printing the first and last zero of the line
+
+# # This won't work
+# # arr = [[0] * 4] * 4

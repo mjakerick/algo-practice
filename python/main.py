bd71ab9a2388dd58280d0851c524f7ee1ec72dd9
Jake <mjakerick@gmail.com>
2023-09-08 09:33:06 -0500

    classes

--- python/main.py
+++ python/main.py
@@ -683,3 +683,29 @@
 # nums = [1, 2]
 # val = 3
 # double(nums, val)
+
+
+# # # # # # # # # #
+# CLASSES
+# # # # # # # # # #
+
+# class MyClass:
+#     # Constructor
+#     def __init__(self, nums):
+#         # Create member variables
+#         self.nums = nums
+#         self.size = len(nums)
+    
+#     # self key word required as param
+#     def getLength(self):
+#         return self.size
+
+#     def getDoubleLength(self):
+#         return 2 * self.getLength()
+
+# myObj = MyClass([1, 2, 3])
+# print(myObj.getLength())
+# print(myObj.getDoubleLength())
+# # result is
+# # 3
+# # 6

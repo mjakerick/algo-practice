eb521f584c2b662ba44b66d78111f82ffd311a76
Jake <mjakerick@gmail.com>
2023-09-08 09:30:26 -0500

    functions

--- python/main.py
+++ python/main.py
@@ -635,3 +635,51 @@
 # # 4
 # # 5
 # # 8
+
+
+# # # # # # # # # #
+# FUNCTIONS
+# # # # # # # # # #
+
+# def myFunc(n, m):
+#     return n * m
+
+# print(myFunc(3, 4))
+# # result is
+# # 12
+
+# # Nested functions have access to outer variables
+# def outer(a, b):
+#     c = "c"
+
+#     def inner():
+#         return a + b + c
+#     return inner()
+
+# print(outer("a", "b"))
+# # result is
+# # abc
+
+# # Can modify objects but not reassign
+# # unless using nonlocal keyword
+# def double(arr, val):
+#     def helper():
+#         # Modifying array works
+#         for i, n in enumerate(arr):
+#             arr[i] *= 2
+        
+#         # will only modify val in the helper scope
+#         # val *= 2
+
+#         # this will modify val outside helper scope
+#         nonlocal val
+#         val *= 2
+#     helper()
+#     print(arr, val)
+# # result is
+# # [2, 4] 6
+
+# # variables
+# nums = [1, 2]
+# val = 3
+# double(nums, val)

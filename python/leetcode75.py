0c781134f6b9973dcd22f32bec5584c11e5289ff
Jake <mjakerick@gmail.com>
2023-09-11 12:00:56 -0500

    Contains Duplicate

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -20,7 +20,7 @@ from collections import deque
 from collections import Counter
 
 # # # # # # # # # #
-# 1. TWO SUM
+# 1. TWO SUM - 1/75
 # # # # # # # # # #
 
 # # Brute Force : Nested For Loops
@@ -35,8 +35,8 @@ from collections import Counter
 # print(Solution().twoSum(nums=[2, 7, 11, 15], target=18))
 # print(Solution().twoSum(nums=[2, 7, 11, 15], target=22))
 
-# # Time : O(n^2)
-# # Memory : O(1) because it doesn't use additional memory that depends on the input size
+# Time : O(n^2)
+# Memory : O(1) because it doesn't use additional memory that depends on the input size
 
 # # Clever Solution : Hash Map
 # class Solution:
@@ -54,28 +54,28 @@ from collections import Counter
 # print(Solution().twoSum(nums=[2, 7, 11, 15], target=18))
 # print(Solution().twoSum(nums=[2, 7, 11, 15], target=22))
 
-# # Time : O(n)
-# # Memory : O(n)
+# Time : O(n)
+# Memory : O(n)
 
-# # Example 1:
+# Example 1:
 
-# #     Input: nums = [2,7,11,15], target = 9
-# #     Output: [0,1]
-# #     Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#     Input: nums = [2,7,11,15], target = 9
+#     Output: [0,1]
+#     Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-# # Example 2:
+# Example 2:
 
-# #     Input: nums = [3,2,4], target = 6
-# #     Output: [1,2]
+#     Input: nums = [3,2,4], target = 6
+#     Output: [1,2]
 
-# # Example 3:
+# Example 3:
 
-# #     Input: nums = [3,3], target = 6
-# #     Output: [0,1]
+#     Input: nums = [3,3], target = 6
+#     Output: [0,1]
 
 
 # # # # # # # # # #
-# 121. Best Time to Buy and Sell Stock
+# 121. Best Time to Buy and Sell Stock - 2/75
 # # # # # # # # # #
 
 # class Solution:
@@ -96,3 +96,111 @@ from collections import Counter
     
 # print(Solution().maxProfit(prices=[7,1,5,3,6,4]))
 # print(Solution().maxProfit(prices=[7,6,4,3,1]))
+
+# Time : O(n)
+# Memory : O(1)
+
+# Example 1:
+
+#     Input: prices = [7,1,5,3,6,4]
+#     Output: 5
+#     Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+#     Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+# Example 2:
+
+#     Input: prices = [7,6,4,3,1]
+#     Output: 0
+#     Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+# # # # # # # # # #
+# 217. Contains Duplicate - 3/75
+# # # # # # # # # #
+
+# # Brute Force
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         for i in range(n - 1):
+#             for j in range(i + 1, n):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+    
+# print(Solution().containsDuplicate(nums = [1,2,3,1]))
+# print(Solution().containsDuplicate(nums=[1,2,3,4]))
+# print(Solution().containsDuplicate(nums=[1,1,1,3,3,4,3,2,4,2]))
+
+# Time : O(n^2)
+# Memory : O(1)
+
+
+# # Sorting
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         n = len(nums)
+#         for i in range(1, n):
+#             if nums[i] == nums[i - 1]:
+#                 return True
+#         return False
+    
+# print(Solution().containsDuplicate(nums = [1,2,3,1]))
+# print(Solution().containsDuplicate(nums=[1,2,3,4]))
+# print(Solution().containsDuplicate(nums=[1,1,1,3,3,4,3,2,4,2]))
+
+# Time : O(nlogn)
+# Memory : O(1)
+
+
+# # Hash Set
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         hashset = set()
+
+#         for n in nums:
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
+#         return False
+    
+# print(Solution().containsDuplicate(nums = [1,2,3,1]))
+# print(Solution().containsDuplicate(nums=[1,2,3,4]))
+# print(Solution().containsDuplicate(nums=[1,1,1,3,3,4,3,2,4,2]))
+
+# Time : O(n)
+# Memory : O(n)
+
+
+# # Hash Map
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         seen = {}
+#         for num in nums:
+#             if num in seen and seen[num] >= 1:
+#                 return True
+#             seen[num] = seen.get(num, 0) + 1
+#         return False
+    
+# print(Solution().containsDuplicate(nums = [1,2,3,1]))
+# print(Solution().containsDuplicate(nums=[1,2,3,4]))
+# print(Solution().containsDuplicate(nums=[1,1,1,3,3,4,3,2,4,2]))
+
+# Time : O(n)
+# Memory : O(n)
+
+# Example 1:
+
+#     Input: nums = [1,2,3,1]
+#     Output: true
+
+# Example 2:
+
+#     Input: nums = [1,2,3,4]
+#     Output: false
+
+# Example 3:
+
+#     Input: nums = [1,1,1,3,3,4,3,2,4,2]
+#     Output: true

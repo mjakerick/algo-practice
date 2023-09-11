0b6f71c94b36f25b70d618a641f08509d2feb095
Jake <mjakerick@gmail.com>
2023-09-10 20:42:20 -0500

    Best Time to Buy and Sell Stock

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -78,21 +78,21 @@ from collections import Counter
 # 121. Best Time to Buy and Sell Stock
 # # # # # # # # # #
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1 # left = buy, right = sell
-        maxP = 0
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         l, r = 0, 1 # left = buy, right = sell
+#         maxP = 0
 
-        while r < len(prices):
-            # profitable?
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
-            else:
-                l = r
-            r += 1
+#         while r < len(prices):
+#             # profitable?
+#             if prices[l] < prices[r]:
+#                 profit = prices[r] - prices[l]
+#                 maxP = max(maxP, profit)
+#             else:
+#                 l = r
+#             r += 1
 
-        return maxP
+#         return maxP
     
-print(Solution().maxProfit(prices=[7,1,5,3,6,4]))
-print(Solution().maxProfit(prices=[7,6,4,3,1]))
+# print(Solution().maxProfit(prices=[7,1,5,3,6,4]))
+# print(Solution().maxProfit(prices=[7,6,4,3,1]))

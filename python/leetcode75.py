5bf26c97694fd82f99b89375e3252d0fda7a1aa6
Jake <mjakerick@gmail.com>
2023-09-12 13:20:39 -0500

    Product of Array Except Self

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -204,3 +204,38 @@ from collections import Counter
 
 #     Input: nums = [1,1,1,3,3,4,3,2,4,2]
 #     Output: true
+
+
+# # # # # # # # # #
+# 238. Product of Array Except Self - 4/75
+# # # # # # # # # #
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         res = [1] * (len(nums))
+
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix *= nums[i]
+#         postfix = 1
+#         for i in range(len(nums) - 1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+#         return res
+
+# print(Solution().productExceptSelf(nums = [1,2,3,4]))
+# print(Solution().productExceptSelf(nums = [-1,1,0,-3,3]))
+
+# Time : O(n)
+# Memory : O(n)
+
+# Example 1:
+
+#     Input: nums = [1,2,3,4]
+#     Output: [24,12,8,6]
+
+# Example 2:
+
+#     Input: nums = [-1,1,0,-3,3]
+#     Output: [0,0,9,0,0]

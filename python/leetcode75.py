dfb055027fea98102b74bfbb9e1f427fb4206703
Jake <mjakerick@gmail.com>
2023-09-13 23:21:20 -0500

    Maximum Subarray

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -239,3 +239,42 @@ from collections import Counter
 
 #     Input: nums = [-1,1,0,-3,3]
 #     Output: [0,0,9,0,0]
+
+
+# # # # # # # # # #
+# 53. Maximum Subarray - 5/75
+# # # # # # # # # #
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+
+print(Solution().maxSubArray(nums = [-2,1,-3,4,-1,2,1,-5,4]))
+print(Solution().maxSubArray(nums = [1]))
+print(Solution().maxSubArray(nums = [5,4,-1,7,8]))
+
+# Example 1:
+
+#     Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+#     Output: 6
+#     Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Example 2:
+
+#     Input: nums = [1]
+#     Output: 1
+#     Explanation: The subarray [1] has the largest sum 1.
+
+# Example 3:
+
+#     Input: nums = [5,4,-1,7,8]
+#     Output: 23
+#     Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.

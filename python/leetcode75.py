6d2b438c69f5bffe52035e40b5cb96cbd3e1b50a
Jake <mjakerick@gmail.com>
2023-09-19 20:48:41 -0500

    11. Container With Most Water - 10/75

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -471,3 +471,49 @@ from collections import Counter
 #     Input: nums = [0,0,0]
 #     Output: [[0,0,0]]
 #     Explanation: The only possible triplet sums up to 0.
+
+
+# # # # # # # # # #
+# 11. Container With Most Water - 10/75
+# # # # # # # # # #
+
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+
+        # # BRUTE FORCE
+        # res = 0
+
+        # for l in range(len(height)):
+        #     for r in range(l + 1, len(height)):
+        #         area = (r - l) * min(height[l], height[r])
+        #         res = max(res, area)
+        # return res
+        
+        
+        # # LINEAR TIME SOLUTION
+        # res = 0
+        # l, r = 0, len(height) - 1
+
+        # while l < r:
+        #     area = (r - l) * min(height[l], height[r])
+        #     res = max(res, area)
+
+        #     if height[l] < height[r]:
+        #         l += 1
+        #     else:
+        #         r -= 1
+        # return res
+
+# print(Solution().maxArea(height = [1,8,6,2,5,4,8,3,7]))
+# print(Solution().maxArea(height = [1,1]))
+
+# Example 1:
+
+#     Input: height = [1,8,6,2,5,4,8,3,7]
+#     Output: 49
+#     Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+# Example 2:
+
+#     Input: height = [1,1]
+#     Output: 1

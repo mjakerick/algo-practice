a8f17def02e2ae4a211c748b326bf602885d2b66
Jake <mjakerick@gmail.com>
2023-09-22 23:11:25 -0500

    not finished

--- python/leetcode75.py
+++ python/leetcode75.py
@@ -554,3 +554,39 @@ from collections import Counter
 
 #         Input: a = -1, b = 1
 #         Output: 0
+
+
+# # # # # # # # # #
+# 191. Number of 1 Bits - 12/75
+# # # # # # # # # #
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            n &= (n - 1)
+            res += 1
+        return res
+
+print(Solution().hammingWeight(n = 00000000000000000000000000001011))
+print(Solution().hammingWeight(n = 00000000000000000000000010000000))
+print(Solution().hammingWeight(n = 11111111111111111111111111111101))
+
+# Example 1:
+
+#         Input: n = 00000000000000000000000000001011
+#         Output: 3
+#         Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
+# Example 2:
+
+#         Input: n = 00000000000000000000000010000000
+#         Output: 1
+#         Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+
+# Example 3:
+
+#         Input: n = 11111111111111111111111111111101
+#         Output: 31
+#         Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+        

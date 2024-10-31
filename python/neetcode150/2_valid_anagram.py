d666ce2ff3bf3a0434cf677d3086f8ecd2281a2c
mjakerick <mjakerick@gmail.com>
2024-10-31 12:34:50 -0400

    2_valid_anagram

--- python/neetcode150/2_valid_anagram.py
+++ python/neetcode150/2_valid_anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        oneSorted = sorted(s)
+        twoSorted = sorted(t)
+        if oneSorted == twoSorted:
+            return True
+        else:
+            return False
         
+print(Solution().isAnagram(s = "racecar", t = "carrace"))
+print(Solution().isAnagram(s = "jar", t = "jam"))
